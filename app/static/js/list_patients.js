7365fec443b3b88007de0be242a5cab180f218da
Vanderson Torres <vanderson.torres06@gmail.com>
2025-03-02 19:06:11 -0300

    flexing patients listing view (show or hidden)

--- app/static/js/list_patients.js
+++ app/static/js/list_patients.js
@@ -1,17 +1,36 @@
 // Redirects to anthro and skinfold managers
 
+// Function to alternate patients view
+function togglePatients(event) {
+    event.preventDefault();
+    var listings = document.getElementById("patients-list");
+    var toggleLink = document.getElementById("toggle-patients");
+
+    if (listings.style.display === "none" || listings.style.display === "") {
+        listings.style.display = "block";
+        toggleLink.textContent = "Esconder pacientes";
+    } else {
+        listings.style.display = "none";
+        // Update the text with the total amount of patients
+        var totalPatients = listings.querySelectorAll("li").length;
+        toggleLink.textContent = "Mostrar " + totalPatients + " pacientes";
+    }
+}
+
+// Redirects to anthro evaluation
 function redirectAnthro(event) {
     event.preventDefault();
     var select = document.getElementById("patient_select");
     var id = select.value;
     if (id) {
-        // Getting the global variable assigned in template
+        // Getting the global variables assigned in template
         window.location.href = addAnthroUrlTemplate.replace("0", id);
     } else {
         alert("Por favor, selecione um paciente.");
     }
 }
 
+// Redirects to skinfolds
 function redirectSkinfold(event) {
     event.preventDefault();
     var select = document.getElementById("patient_select_skinfold");
